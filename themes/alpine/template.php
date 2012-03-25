978f79b46344feb8b56ee5623b9b8bfa85a22601
Jesper Kristensen <cableman@linuxdev.dk>
2012-03-25 15:05:36 +0200

    Updated foto features

--- themes/alpine/template.php
+++ themes/alpine/template.php
@@ -12,7 +12,30 @@
  */
 
 function alpine_preprocess_page(&$vars) {
+  // Get the front page slider up-and-running.
   drupal_add_js(drupal_get_path('theme', 'alpine') . '/js/jquery.flexslider-min.js');
   drupal_add_js(drupal_get_path('theme', 'alpine') . '/js/slider.js');
   drupal_add_css(drupal_get_path('theme', 'alpine') . '/css/flexslider.css');
+}
+
+/**
+ * Implements hook_breadcrumb().
+ *
+ * Remove the current page title from the breadcrumb.
+ */
+function alpine_breadcrumb($variables) {
+  $output = '';
+  $breadcrumb = $variables['breadcrumb'];
+
+    // Build breadcrumb.
+  if (!empty($breadcrumb)) {
+    $breadcrumb[] = drupal_get_title();
+  }
+
+  // Provide a navigational heading to give context for breadcrumb links to
+  // screen-reader users. Make the heading invisible with .element-invisible.
+  $output = '<h2 class="element-invisible">' . t('You are here') . '</h2>';
+  $output .= '<nav class="breadcrumb">' . implode(' > ', $breadcrumb) .'</nav>';
+
+  return $output;
 }
